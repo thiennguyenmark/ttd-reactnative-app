48eec8a6f90e3642871923a46a063f999fbff91c
thiennguyendev <thiennguyen.mark@gmail.com>
2019-03-21 21:21:44 +0700

    Check git

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Main UI</Text>
+        <Text>Main UI</Text>
       </View>
     );
   }
